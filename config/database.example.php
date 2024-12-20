61a5fbd10d3f961e47d99b57dea8e1f346de763d
PypNetty <henryck.paris@outlook.fr>
2024-12-20 22:28:02 +0100

    Update database example configuration

--- config/database.example.php
+++ config/database.example.php
@@ -1,1 +1,11 @@
- 
+<?php
+return [
+    'dsn' => 'mysql:host=localhost;dbname=health_north;charset=utf8mb4',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'options' => [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]
+]; 
